ce57ce6f947805de1177a5e0b70527eff06edb59
enerjay <enerjay@gmail.com>
2015-11-02 15:26:25 +0000

    created the home comtroller

--- musart/app/controllers/users_controller.rb
+++ musart/app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :full_name, :bio, :email, :profile_image)
+      params.require(:user).permit(:username, :full_name, :bio, :email, :profile_image, :style, :category,)
     end
 end
--- musart/config/routes.rb
+++ musart/config/routes.rb
@@ -3,7 +3,16 @@ Rails.application.routes.draw do
   resources :projects
   resources :users
 
-  root "home#index"
+  root "static#home"
+    
+    
+    get "Projects", to: "projects#index"
+
+    get "Users", to: "users#index"
+
+   
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
