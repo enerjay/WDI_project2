1b513473a6a8ba0cc52c0187a4f0d39af08851d0
enerjay <enerjay@gmail.com>
2015-11-02 12:16:17 +0000

    added the styles and categories in the project model

--- musart/app/models/project.rb
+++ musart/app/models/project.rb
@@ -1,2 +1,8 @@
 class Project < ActiveRecord::Base
+  belongs_to :user
+
+  STYLES = ["Experimental", "Video Mapping", "Drawn on Film", "Figurative", "3D"]
+  CATEGORIES = ["Installation", "Video Art", "Music Festival", "Performance", "Music Video", "Dev art"]
+
+
 end
