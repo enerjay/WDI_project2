3aad8b92f50c17d4c18c24fd27c9e6c843fef8f0
enerjay <enerjay@gmail.com>
2016-01-06 12:15:16 +0000

    moved all rails app content to root folder level

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -37,7 +37,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :detail do
-    process :resize_to_fit => [600, 600]
+    process :resize_to_fit => [500, 500]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
